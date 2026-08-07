0f919675fc9a2de02c3b8f9043b6f239e515daac
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-08-07 22:46:25 +0530

    create login function

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -70,3 +70,58 @@ module.exports={
 register
 
 };
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid Credentials"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login Successful",
+            token
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+module.exports = {
+    register,
+    login
+};
