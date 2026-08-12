5d56d1feb56191408df1bb5236bc0e242aa460fc
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-08-12 17:25:51 +0530

    modify for middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,16 @@ const router = express.Router();
 
 const {
     register,
-    login
+    login,
+    getMe
 } = require("../controllers/authControllers");
+
+const protect = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/me", protect, getMe);
 
 module.exports = router;
