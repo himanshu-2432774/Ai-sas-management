5d3329efe4fd38797cf425961ecc40682d2ea69c
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-08-22 22:04:06 +0530

    create profile controllers

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+
+const getProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id)
+            .select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Profile fetched successfully",
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
+};
+
+module.exports = {
+    getProfile
+};
