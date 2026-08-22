fdf3b11451ee0af846029e688100e23dd6378765
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-08-22 23:54:52 +0530

    add the update profile features

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -23,7 +23,44 @@ const getProfile = async (req, res) => {
         });
     }
 };
+const updateProfile = async (req, res) => {
+    try {
+        const { name, email } = req.body;
 
+        const user = await User.findById(req.user.id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        if (name) {
+            user.name = name;
+        }
+
+        if (email) {
+            user.email = email;
+        }
+
+        await user.save();
+
+        const updatedUser = await User.findById(req.user.id)
+            .select("-password");
+
+        res.status(200).json({
+            message: "Profile updated successfully",
+            user: updatedUser
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
+};
 module.exports = {
-    getProfile
+    getProfile,
+    updateProfile
 };
