288f512e2de257f99d60f247833de609001ed729
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-03 22:40:51 +0530

    update the airoutes.js

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,26 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
 
-const protect = require("../middleware/authmiddleware.js");
+const { protect } = require("../middleware/authmiddleware");
 const checkCredits = require("../middleware/creditMiddleware");
+const { generateAI } = require("../controllers/aiController");
 
 router.post(
     "/generate",
     protect,
     checkCredits,
-    async (req, res) => {
-
-        req.userData.credits -= 1;
-
-        await req.userData.save();
-
-        res.status(200).json({
-            message: "AI request successful",
-            remainingCredits: req.userData.credits
-        });
-
-    }
+    generateAI
 );
 
 module.exports = router;
