326613409da0f8e73a0846cf6aaad5beee3bbe68
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-04 19:41:38 +0530

    create Statistics route

--- server/routes/usageRoutes.js
+++ server/routes/usageRoutes.js
@@ -3,12 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
-const { getMyUsage } = require("../controllers/usageController");
+
+const {
+    getMyUsage,
+    getUsageStats
+} = require("../controllers/usageController");
 
 router.get(
     "/",
     protect,
     getMyUsage
+);
+
+router.get(
+    "/stats",
+    protect,
+    getUsageStats
 );
 
 module.exports = router;
