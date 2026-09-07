c5804fb28cbcf342c262a83c85df54071ec1d51d
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-07 23:12:12 +0530

    fix the error

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const protect = require("../middleware/authmiddleware.js");
 const checkCredits = require("../middleware/creditmiddleware.js");
 const checkSubscription = require("../middleware/subscriptionMiddleware.js");
-const aiRateLimiter = require("../middleware/aiRateLimiter.js");
+const aiRateLimiter = require("../middleware/airateLimiter.js");
 
 const {
     generateAI,
