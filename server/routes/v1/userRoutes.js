6392ffdb62d9dccffb5821195f6907a74f74c2a7
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-09 23:47:19 +0530

    fix the error

--- server/routes/v1/userRoutes.js
+++ server/routes/v1/userRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getProfile } = require("../../controllers/userController");
-const { protect } = require("../../middleware/authMiddleware");
+const { protect } = require("../../middleware/authmiddleware.js");
 
 router.get("/profile", protect, getProfile);
 
