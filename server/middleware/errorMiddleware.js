f57acc7ced174eda8ce5079de1b3c80dac12af43
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-11 18:55:18 +0530

    improve it

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,7 +1,12 @@
+
 const errorHandler = (err, req, res, next) => {
+
     console.error(err.stack);
 
-    res.status(err.statusCode || 500).json({
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
         message: err.message || "Internal Server Error"
     });
 };
