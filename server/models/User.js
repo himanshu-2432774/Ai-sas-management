eed3b3d6541b8893ec7917efd3baee200f7a5082
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-15 10:54:41 +0530

    add index into user schema

--- server/models/User.js
+++ server/models/User.js
@@ -93,5 +93,7 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 );
+userSchema.index({ role: 1 });
+userSchema.index({ createdAt: -1 });
 
 module.exports = mongoose.model("User", userSchema);
