fcf0f8d4c4a66b6886e3e9cd18eb67792d8723bf
himanshu-2432774 <himanshu.2432774@gmail.com>
2026-09-17 23:13:10 +0530

    limit json request size

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({
+    limit: "10kb"
+}));
 // add logger
 const logger = require("./middleware/loggerMiddleware");
 app.use(logger);
